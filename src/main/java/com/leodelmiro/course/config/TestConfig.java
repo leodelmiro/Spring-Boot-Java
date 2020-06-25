5a0ef19608d67aa1d4e6d3a85e3e27bc479076b7
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-06-25 20:58:28 -0300

    Service layer, component registration

--- src/main/java/com/leodelmiro/course/config/TestConfig.java
+++ src/main/java/com/leodelmiro/course/config/TestConfig.java
@@ -4,6 +4,7 @@ import com.leodelmiro.course.entities.User;
 import com.leodelmiro.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -11,11 +12,11 @@ import java.util.Arrays;
 
 @Configuration
 @Profile("test")
+
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
-
 
     @Override
     public void run(String... args) throws Exception {
--- src/main/java/com/leodelmiro/course/repositories/UserRepository.java
+++ src/main/java/com/leodelmiro/course/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.leodelmiro.course.repositories;
 
 import com.leodelmiro.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
