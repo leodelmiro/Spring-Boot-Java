00c225d61b166d9ef255dea7c4d4853eb105ebcd
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-06-28 11:37:27 -0300

    Product

---
+++ src/main/java/com/leodelmiro/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.leodelmiro.course.repositories;
+
+import com.leodelmiro.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
