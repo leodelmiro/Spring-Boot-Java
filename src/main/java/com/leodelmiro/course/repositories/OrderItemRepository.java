70b191e8d148c0737ac07b257d1ea0e496529a37
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-06-29 21:35:00 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/leodelmiro/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.leodelmiro.course.repositories;
+
+import com.leodelmiro.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
